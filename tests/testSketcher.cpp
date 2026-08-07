e925da6c28825667ed3d1ab488f55705cf86fd56
Zachery Dickson <zachery.dickson@helsinki.fi>
2026-08-07 15:46:17 +0300

    Implemented tests for sketch similarity behaviour and downsampling behaviour

--- tests/testSketcher.cpp
+++ tests/testSketcher.cpp
@@ -4,11 +4,14 @@
 #include <catch2/catch_all.hpp>
 #include <Catch2Extensions.hpp>
 #include <algorithm>
+#include <random>
+#include <unordered_set>
 
 
 Sketcher::Alphabet alpha2 = {{'A',0},{'C',1}};
 Sketcher::Alphabet alpha3 = {{'A',0},{'C',1},{'G',2}};
 Sketcher::Alphabet alpha4 = {{'A',0},{'C',1},{'G',2},{'T',3}};
+std::map<uint8_t,char> bit2nuc4 = {{0,'A'},{1,'C'},{2,'G'},{3,'T'}};
 
 
 TEST_CASE( "LexicographicCoding" , "[Sketch]") {
@@ -88,71 +91,187 @@ TEST_CASE( "Correct Sketches can be generated", "[Sketch]") {
            {   {0xC0DEB678B0D9E8AD,2},
                {0xE61F35FF38889D0D,3},
                {0xAD5B20031CE9F118,4} } },
-       {    "ACGATGCTACTTGACGT",
-            Sketcher(8,5,1.0,4,[](std::string_view sv){
-                    return (size_t) std::hash<std::string_view>{}(sv); }),
-            {   {0x3676e1ac56f7b2eb,1},
-                {0xc6a6dedf91006e09,2},
-                {0xc23740410cd80b2b,5},
-                {0x867f1fe436983e24,6},
-                {0xc242b6e4fd5ba2a4,9} } },
         {
             "GTCAGTCGTAGCTAGCTGACTGCAT",
             Sketcher(17,5,1.0,Sketcher::DNA_Alphabet),
             {   {0x00000001B2727879,7}
             }
+        },
+        { // Can't generate a sketch for a equence smaller than k
+            "ACTG",
+            Sketcher(10,4,1.0),
+            {}
         }
     }) );
 
+    INFO("The input sequence is " << seq);
     Sketch sketch = sketcher.generate_sketch(seq);
+    REQUIRE (sketch.size() == expected.size());
     REQUIRE( std::equal(sketch.begin(),sketch.end(),expected.begin()));
- //   Sketcher sketcher();
- //   std::equal()
- //   static const int nSeq = 2;
- //   std::string seqs[nSeq] = {  "ACTGACTGGATCAGAACAGGGTGAGAT",
- //                               "GTACCGATACGTACGACGTACGTCAGT" };
-    //auto i 
-    //GIVEN( "An FNV Hash Sketcher") {
-    //    Sketcher(15,5,1.0),
-    //    WHEN( "Sketches are generated" ) {
-    //        Sketch sketches[nSeq];
-    //        for(int i = 0; i < nSeq; i++){
-    //            sketches[i] = Sketcher.generate_
-    //    }
-    //}
-    //static const int nSketcher = 3;
-    //Sketcher sketchers[nSketcher] = {
-    //                            Sketcher(15,6,1.0,Sketcher::DNA_Alphabet),
-    //                            Sketcher(21,5,1.0,4,stdHasher) };
-    //Sketch sketches[nSeq * nSketcher];
-    //for(int i = 0; i < nSeq; i++){
-    //    for (int j = 0; j < nSketcher; j++){
-    //        sketches[i*nSeq+j] = sketchers[j].generate_sketch(seqs[i]);
-    //    }
-    //}
-    //SECTION("Generates correct Sketches") {
-    //    REQUIRE(
-    //}
-    //Sketch sketch = sketcher.generate_sketch(seq);
-    //REQUIRE(sketch.size() > 0);
 }
 
-//
-//TEST_CASE( "Hash function initialization works", "[Sketch]" ) {
-//    Sketcher defaultSketcher(15,5,1.0);
-//    Sketcher::HashFunction F([](std::string_view a ) {
-//            return std::hash<std::string_view>{}(a);});
-//    Sketcher stdhashSketcher(15,5,1.0,4,F);
-//    std::string seq = "ACTGACTGGATCAGAACAGGG";
-//    Sketch defaultSketch = defaultSketcher.generate_sketch(seq);
-//    Sketch stdhashSketch = defaultSketcher.generate_sketch(seq);
-//
-//    for(auto & elem : defaultSketch){
-//        std::cout << elem.position << "\t";
-//    }
-//    std::cerr << "\n";
-//    for(auto & elem : stdhashSketch){
-//        std::cout << elem.position << "\t";
-//    }
-//    std::cerr << "\n";
-//}
+TEST_CASE("Downsampling occurs correctly") {
+    std::string seq = "ACTGACTGGA";
+    const Sketch Expected =
+           {   {0xC0DEB678B0D9E8AD,2},
+               {0xE61F35FF38889D0D,3},
+               {0xAD5B20031CE9F118,4} };
+    auto [dsfactor, expIdxVec] = GENERATE(table<double, std::vector<int>>({
+                {1.0, {0,1,2}},
+                {2.0, {2}},
+                {7.0, {}},
+                {19.0, {1}} } ) );
+    Sketcher sketcher(6,4,dsfactor);
+    Sketch sketch = sketcher.generate_sketch(seq);
+    REQUIRE(sketch.size() == expIdxVec.size());
+    for(size_t i = 0; i < sketch.size(); i++){
+        REQUIRE(sketch[i] == Expected[expIdxVec[i]]);
+    }
+}
+
+//Assumes that the question is: how much of a does b cover?
+double SketchSimilarity(const Sketch & a, const Sketch & b) {
+    size_t intersect = 0;
+    auto it_a = a.begin();
+    auto it_b = b.begin();
+    while(it_a != a.end() && it_b != b.end()){
+        if(*it_a == *it_b){
+            intersect++;
+            it_a++;
+            it_b++;
+        } else if(it_a->position > it_b->position){
+            it_b++;
+        } else {
+            it_a++;
+        }
+    }
+    double ji = double(intersect) / double(a.size());
+    return ji;
+}
+
+double SketchSimilarity( const Sketch & a, const std::string bStr,
+                                const Sketcher & sketcher)
+{
+    Sketch b = sketcher.generate_sketch(bStr);
+    return SketchSimilarity(a,b);
+}
+
+std::string RandomDNA(size_t length) {
+    std::string seq;
+    while(seq.length() < length){
+        int64_t byte = rand();
+        for(int i =0; i < 16; i++){
+            seq += bit2nuc4[byte & 3];
+            byte >>= 2;
+        }
+    }
+    return  seq.substr(0,length);
+}
+
+std::string CompositionMatchedRandomDNA(const std::string & other) {
+    std::string seq(other);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    while(seq == other){
+        std::shuffle(seq.begin(),seq.end(),g);
+    }
+    return seq;
+}
+
+//nVar caps out at half the sequence length (rounded up)
+std::string MutatedRandomDNA(   const std::string & other, size_t nVar,
+                                std::unordered_set<int> * seenPos_pt = NULL )
+{
+    std::string seq(other);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::uniform_int_distribution<> rpos(0,seq.length()-1);
+    std::uniform_int_distribution<> rmut(1,3);
+    size_t maxVar = std::ceil(other.length() * 0.5);
+    if(nVar > maxVar){
+        nVar = maxVar;
+    }
+    //Local default
+    std::unordered_set<int> seenPos;
+    if(!seenPos_pt){
+        seenPos_pt = & seenPos;
+    }
+    for(size_t i = 0; i < nVar; i++){
+        int pos;
+        //Select a position which has not yet been changed
+        do {
+            pos = rpos(g);
+        } while(seenPos_pt->count(pos));
+        seenPos_pt->insert(pos);
+        uint8_t bits = Sketcher::DNA_Alphabet.at(other[pos]);
+        char nuc = (bits + rmut(g)) % 4;
+        seq[pos] = nuc;
+    }
+    return seq;
+}
+
+double factorial(double n) {
+    double res = 1;
+    for(size_t i = 2; i <= n; i++){
+        res *= i;
+    }
+    return res;
+}
+
+//This probability assumes all characters in l are distinct
+//Which operates as a conservative lower bound for the the probability
+//with repeated characters
+//l is the length of the permuted sequence 
+//k is the number of positions which do not change
+double ProbPermutedPass(size_t l, size_t k) {
+    double p = 0.0;
+    for(size_t m = 0; m <= k; m++){
+        double derange = 0;
+        for(size_t i = 0; i <= m; i++){
+            derange += ((i % 2 == 1) ? -1 : 1) / factorial(i);
+        }
+        p += derange / factorial(l - m);
+    }
+    return p;
+}
+
+
+TEST_CASE ("Sketches from mutated duplicates are usually more similar than shuffled seqs", "[Sketcher]") {
+    auto seqLen = GENERATE(25,take(3, random(50, 150)));
+    auto sketcher = GENERATE(Sketcher(13,7,2.0),Sketcher(17,8,1.0));
+    std::string templateSeq = RandomDNA(seqLen);
+    Sketch templateSketch = sketcher.generate_sketch(templateSeq);
+
+    size_t nScenario = 10;
+    double expectedFalseFail = 0;
+    size_t nFail = 0;
+    for(size_t i = 0; i < nScenario; i++){
+        std::string mutatedSeq = templateSeq;
+        std::unordered_set<int> seenPos;
+        double lastSim = 1.0;
+        size_t lastNVar = 0;
+        for(double varProp = 0; varProp < 0.5; varProp += 0.05){
+            size_t nVar = std::ceil(varProp * seqLen);
+            if(nVar == 0) { nVar++; }
+            if(nVar <= lastNVar) { continue; }
+            double probFalseFail = ProbPermutedPass(seqLen,nVar);
+            expectedFalseFail += probFalseFail;
+            mutatedSeq = MutatedRandomDNA(mutatedSeq,nVar-lastNVar,&seenPos);
+            double mutatedSim = SketchSimilarity( templateSketch,
+                                                  mutatedSeq,
+                                                  sketcher );
+            lastNVar = nVar;
+            CHECK(mutatedSim <= lastSim);
+            lastSim = mutatedSim;
+            double matchedSim = SketchSimilarity(
+                                templateSketch,
+                                CompositionMatchedRandomDNA(templateSeq),
+                                sketcher );
+            if(mutatedSim < matchedSim){
+                nFail++;
+            }
+        }
+    }
+    INFO("The number of failures was " << nFail << " compared to " << expectedFalseFail);
+    REQUIRE(nFail < std::ceil(expectedFalseFail)  );
+}
