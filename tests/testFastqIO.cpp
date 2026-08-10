71b14064374c8674b73b72a5fd031258813c2bc5
Zachery Dickson <zachery.dickson@helsinki.fi>
2026-08-10 18:27:24 +0300

    Added infrastructure for testing FastqIO

---
+++ tests/testFastqIO.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <FastqIO.h>
+#include <string>
+#include <catch2/catch_all.hpp>
+#include <Catch2Extensions.hpp>
+
+TEST_CASE ("Dummy Test", "[FastqIO]") {
+    SUCCEED(" dummy");
+}
