034dd7a12914f14fe94348b064137d964d02c174
Zachery Dickson <zachery.dickson@helsinki.fi>
2026-08-13 12:28:58 +0300

    Added tests for FastqIO with temporary files

--- tests/testFastqIO.cpp
+++ tests/testFastqIO.cpp
@@ -1,10 +1,13 @@
+#include <catch2/catch_all.hpp>
+#include <Catch2Extensions.hpp>
 #include <FastqIO.h>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <memory>
 #include <sstream>
 #include <string>
-#include <catch2/catch_all.hpp>
-#include <Catch2Extensions.hpp>
+#include <unistd.h>
 
 
 FastqTemplate_t initFqt1pair = { "Read1",
@@ -31,6 +34,8 @@ FastqTemplate_t initFqtBadLenfwd = { initFqtBadLenpair.name,
 FastqTemplate_t initFqtBadLenrev = { initFqtBadLenpair.name,
     { initFqtBadLenpair.segVec[1] } };
 
+std::string FakeFileStem1 = "NoT_A_rEaL_F1Le";
+std::string FakeFileStem2 = "NoT_A_rEaL_F2Le";
 
 std::unique_ptr<std::iostream > constructSS(
         std::vector<FastqTemplate_t> initVec = {}, bool postappend=false,
@@ -57,6 +62,48 @@ endloop:
         ss_ptr->seekg(0);
     }
     return ss_ptr;
+}
+
+class TempFile {
+public:
+    explicit TempFile(std::string_view contents, std::string ext)
+    {
+        auto path =
+            std::filesystem::temp_directory_path() / ("FileIO-test-XXXXXX" + ext);
+
+        std::string filename = path.string();
+
+        int fd = mkstemps(filename.data(),ext.length());
+        if (fd == -1)
+            throw std::runtime_error("mkstemps failed");
+
+        ::close(fd);
+
+        path_ = std::move(filename);
+
+        std::ofstream out(path_);
+        if (!out)
+            throw std::runtime_error("failed to open temp file");
+
+        out << contents;
+    }
+
+    ~TempFile()
+    {
+        std::error_code ec;
+        std::filesystem::remove(path_, ec);
+    }
+
+    const std::filesystem::path& path() const { return path_; }
+
+private:
+    std::filesystem::path path_;
+};
+
+TempFile constructTF(std::string ext, std::vector<FastqTemplate_t> initVec = {}, bool postappend=false,
+        FastqIO::READ_RESULT res = FastqIO::READ_PASS) {
+    auto ss = constructSS(initVec,postappend,res);
+    return TempFile(dynamic_cast<std::stringstream*>(ss.get())->str(),ext);
 }
 
 SCENARIO ("Object Construction from Injection", "[FastqIO][Construction]") {
@@ -88,7 +135,68 @@ SCENARIO ("Object Construction from Injection", "[FastqIO][Construction]") {
     }
 }
 
-//TODO: Test construction from files
+SCENARIO ("Input handler construction from non-existent files", "[FastqIO][Construction]") {
+    auto ext = GENERATE(".fq",".fq.gz");
+    INFO( "and Given: ext is " << ext );
+    TempFile tf = constructTF(ext);
+    GIVEN("Construction with a single input file") {
+        bool bInterleaved = GENERATE(true,false);
+        INFO("And Given: bInterleaved is " << bInterleaved);
+        THEN("Object construction throws invalid argument") {
+            REQUIRE_THROWS_AS(
+                    FastqIO(FakeFileStem1 + ext,FastqIO::IO_IN, bInterleaved) ,
+                std::invalid_argument );
+        }
+    }
+    GIVEN("Construction with two input files"){
+        auto missingFlag = GENERATE(0b01,0b10,0b11);
+        INFO( "and Given: missingFlag is " << missingFlag );
+        std::string path1 = (missingFlag & 0b10) ? FakeFileStem1 + ext : tf.path().string();
+        std::string path2 = (missingFlag & 0b01) ? FakeFileStem2 +ext : tf.path().string();
+        THEN("Object construction throws invalid argument") {
+            REQUIRE_THROWS_AS(  FastqIO(path1,path2,FastqIO::IO_IN) ,
+                                std::invalid_argument );
+        }
+    }
+}
+
+SCENARIO("Handler Construction with valid files", "[FastqIO][Construction]" ) {
+    auto ext = GENERATE(".fq",".fq.gz");
+    INFO( "and Given: ext is " << ext );
+    auto mode = GENERATE(FastqIO::IO_IN, FastqIO::IO_OUT);
+    INFO("and Given: mode is " << ( (mode == FastqIO::IO_IN) ? "IO_IN" : "IO_OUT"));
+    GIVEN("Construction with a single file") {
+        bool bInterleaved = GENERATE(true,false);
+        INFO("And Given: bInterleaved is " << bInterleaved);
+        TempFile tf = constructTF(ext,{initFqt1pair});
+        std::string path = tf.path().string();
+        THEN("The object can be constructed") {
+            std::unique_ptr<FastqIO> handler_ptr;
+            REQUIRE_NOTHROW( handler_ptr = std::make_unique<FastqIO>(path,mode,bInterleaved) );
+            AND_THEN("The Handler supports the correct operations") {
+                REQUIRE( handler_ptr->canWrite() == (mode == FastqIO::IO_OUT) );
+                REQUIRE( handler_ptr->canRead() == (mode == FastqIO::IO_IN));
+            }
+        }
+    }
+    GIVEN("Construction with a pair of files") {
+        TempFile tf1 = constructTF(ext,{initFqt1fwd});
+        TempFile tf2 = constructTF(ext,{initFqt1rev});
+        std::string path1 = tf1.path().string();
+        std::string path2 = tf2.path().string();
+        THEN("The object can be constructed") {
+            std::unique_ptr<FastqIO> handler_ptr;
+            REQUIRE_NOTHROW( handler_ptr = std::make_unique<FastqIO>(path1,path2,mode) );
+            AND_THEN("The Handler supports the correct operations") {
+                REQUIRE( handler_ptr->canWrite() == (mode == FastqIO::IO_OUT) );
+                REQUIRE( handler_ptr->canRead() == (mode == FastqIO::IO_IN));
+            }
+        }
+    }
+}
+
+//NOTE: It is assumed that, regardless of whether a handler was created from a file or
+// from injection, its internal operations are consistent (and its easier to test injection)
 
 SCENARIO ("Move Construction", "[MoveConstruction][FastqIO][Construction]") {
     auto mode = GENERATE(FastqIO::IO_IN, FastqIO::IO_OUT);
@@ -375,28 +483,74 @@ SCENARIO ( "Releasing Injected Streams","[FastqIO][Injection]") {
 }
 
 SCENARIO ("Round Trip with injected streams", "[FastqIO][Reading][Writing]") {
-    GIVEN("A FastqTemplate with one segment") {
-        WHEN("The template is written to an input handler"){
-            FastqIO out(constructSS({}), FastqIO::IO_OUT);
-            REQUIRE( out.canWrite() );
-            REQUIRE ( out.write(initFqt1fwd) );
+    auto [nStream, bInterleaved] = GENERATE(table<int,bool>({
+                {1,false}, {1,true}, {2,false} } ));
+    INFO("Given: mode (nStream,bInterleaved) is (" << nStream << ", " << bInterleaved << ")" );
+    GIVEN("A valid output handler") {
+        std::unique_ptr<FastqIO> out_ptr ( (nStream == 1) ?
+            new FastqIO(constructSS(),FastqIO::IO_OUT,bInterleaved) :
+            new FastqIO(constructSS(),constructSS(),FastqIO::IO_OUT) );
+        REQUIRE( out_ptr->canWrite() );
+        WHEN("The template is successfully written to an output handler"){
+            REQUIRE ( out_ptr->write(initFqt1pair) );
             AND_WHEN("An input handler is constructed from the released stream"){
-                FastqStreamPair_t sp = std::move(out).releaseStreams();
+                FastqStreamPair_t sp = std::move(*out_ptr).releaseStreams();
                 REQUIRE( sp.first );
+                REQUIRE( bool(sp.second) == (nStream > 1) );
                 sp.first->seekg(0);
-                FastqIO in(std::move(sp.first),FastqIO::IO_IN);
-                REQUIRE(in.canRead());
-                AND_WHEN("A template is read back") {
+                INFO("Written stream1 is: '\n" << dynamic_cast<std::stringstream*>(sp.first.get())->str() << "\n'");
+                if(sp.second){ 
+                    sp.second->seekg(0);
+                    INFO("Written stream2 is: '\n" << dynamic_cast<std::stringstream*>(sp.second.get())->str() << "\n'");
+                }
+                std::unique_ptr<FastqIO> in_ptr ( (nStream == 1) ?
+                    new FastqIO(std::move(sp.first),FastqIO::IO_IN,bInterleaved) :
+                    new FastqIO(std::move(sp.first),std::move(sp.second),FastqIO::IO_IN));
+                REQUIRE(in_ptr->canRead());
+                AND_WHEN("A template is successfully read back") {
                     FastqTemplate_t fqt;
-                    REQUIRE( in.next_template(fqt) == FastqIO::READ_PASS );
+                    REQUIRE( in_ptr->next_template(fqt) == FastqIO::READ_PASS );
                     INFO("the read is " << fqt.to_string() << "\n");
                     THEN("The template matches the original input") {
-                        REQUIRE ( fqt == initFqt1fwd );
+                        REQUIRE ( fqt == ( (in_ptr->isPaired()) ? initFqt1pair : initFqt1fwd) );
                     }
                 }
             }
         }
     }
-    //TODO round trip in other cases
 }
 
+SCENARIO ("Round Trip with temporary files", "[FastqIO][Reading][Writing]") {
+    auto ext = GENERATE(".fq",".fq.gz");
+    INFO( "and Given: ext is " << ext );
+    auto [nStream, bInterleaved] = GENERATE(table<int,bool>({
+                {1,false}, {1,true}, {2,false} } ));
+    INFO("and Given: mode (nStream,bInterleaved) is (" << 
+            nStream << ", " << bInterleaved << ")" );
+    TempFile tf1 = constructTF(ext);
+    TempFile tf2 = constructTF(ext);
+    GIVEN("A valid output handler") {
+        std::unique_ptr<FastqIO> out_ptr ( (nStream == 1) ?
+            new FastqIO(tf1.path().string(),FastqIO::IO_OUT,bInterleaved) :
+            new FastqIO(tf1.path().string(),tf2.path().string(),FastqIO::IO_OUT) );
+        REQUIRE( out_ptr->canWrite() );
+        WHEN("The template is successfully written to an output handler"){
+            REQUIRE ( out_ptr->write(initFqt1pair) );
+            out_ptr.reset(NULL);
+            AND_WHEN("An input handler is constructed from the files"){
+                std::unique_ptr<FastqIO> in_ptr ( (nStream == 1) ?
+                    new FastqIO(tf1.path().string(),FastqIO::IO_IN,bInterleaved) :
+                    new FastqIO(tf1.path().string(),tf2.path().string(),FastqIO::IO_IN));
+                REQUIRE(in_ptr->canRead());
+                AND_WHEN("A template is successfully read back") {
+                    FastqTemplate_t fqt;
+                    REQUIRE( in_ptr->next_template(fqt) == FastqIO::READ_PASS );
+                    INFO("the read is " << fqt.to_string() << "\n");
+                    THEN("The template matches the original input") {
+                        REQUIRE ( fqt == ( (in_ptr->isPaired()) ? initFqt1pair : initFqt1fwd) );
+                    }
+                }
+            }
+        }
+    }
+}
