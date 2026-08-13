dac4a261763b6a628f39ac52f676c5b0e97d9c89
Zachery Dickson <zachery.dickson@helsinki.fi>
2026-08-13 15:06:25 +0300

    Added SketchHashFactory as a library

---
+++ src/SketchHashFactory.cpp
@@ -0,0 +1,4 @@
+#include "SketchHashFactory.h"
+
+SketchHashFactory::SketchHashFactory() {
+}
---
+++ src/SketchHashFactory.h
@@ -0,0 +1,20 @@
+#ifndef SKETCH_HASH_FACTORY_HEADER_GAURD_
+#define SKETCH_HASH_FACTORY_HEADER_GAURD_
+
+#include "Sketcher.h"
+#include "LocalSyncmerMap.h"
+#include "FastqIO.h"
+
+
+struct HashedFastqSet {
+
+};
+
+class SketchHashFactory {
+    SketchHashFactory();
+};
+
+#endif // SKETCH_HASH_FACTORY_HEADER_GAURD_
+
+
+
