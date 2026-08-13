028a0efde8a9fe541f5921295eb840e4125b33b6
Zachery Dickson <zachery.dickson@helsinki.fi>
2026-08-13 18:41:48 +0300

    Initial implementation of SketchHashFactory

--- src/SketchHashFactory.cpp
+++ src/SketchHashFactory.cpp
@@ -1,4 +1,98 @@
 #include "SketchHashFactory.h"
+#include <future>
 
-SketchHashFactory::SketchHashFactory() {
+SketchHashFactory::SketchHashFactory(Sketcher && sketcher, size_t nThread,
+        int phredOffset)
+    :   sketcher_(std::move(sketcher)), threadPool_(nThread),
+        phredOffset_(phredOffset)
+{
+}
+
+
+SketchPair SketchHashFactory::GeneratePairedSketch(const FastqTemplate_t & fqt)
+{
+#ifndef NDEBUG
+    if(fqt.segVec.size() == 0) {
+        throw std::invalid_argument("Attempt to generate paired sketch with an incomplete fastq template");
+    }
+#endif
+    SketchPair sp;
+    sp.first = sketcher_.generate_sketch(fqt.segVec[0].seq);
+    if(fqt.segVec.size() > 1 ){
+        sp.second = sketcher_.generate_sketch(fqt.segVec[1].seq);
+    }
+    return sp;
+}
+
+double SketchHashFactory::CalculateMeanQuality(const FastqTemplate_t & fqt) {
+    double mean = 0.0;
+    size_t n = 0;
+    for(const auto & seg : fqt.segVec) {
+        n += seg.qual.size();
+        for(char q : seg.qual) {
+           mean += q - this->phredOffset_; 
+        }
+    }
+    mean /= double(n);
+    return mean;
+}
+
+
+HashedFastqSet SketchHashFactory::BuildHashedFastqSet(FastqIO & in) {
+    if(threadPool_.size() == 0) {
+        return NoWorkerBuild(in);
+    } else {
+        return ParallelBuild(in);
+    }
+}
+
+
+HashedFastqSet SketchHashFactory::NoWorkerBuild(FastqIO & in) {
+    HashedFastqSet hfqSet;
+    FastqIO::READ_RESULT res = (in.canRead() ? FastqIO::READ_PASS : FastqIO::READ_EOF);
+    while( res == FastqIO::READ_PASS) {
+        size_t fqtIdx = hfqSet.templateVec.size();
+        hfqSet.templateVec.emplace_back();
+        ExtendedFastqTemplate_t & fqt = hfqSet.templateVec.back();
+        res = in.next_template(fqt);
+        if(res == FastqIO::READ_PASS) {
+            const SketchPair & sp = GeneratePairedSketch(fqt);
+            fqt.meanQual = CalculateMeanQuality(fqt);
+            hfqSet.sketchMap.insert(2*fqtIdx+0,sp.first);
+            hfqSet.sketchMap.insert(2*fqtIdx+1,sp.second);
+        } else if(res != FastqIO::READ_EOF){
+            throw std::invalid_argument("Improperly formatted fastq input");
+        }
+    }
+    return hfqSet;
+}
+
+HashedFastqSet SketchHashFactory::ParallelBuild(FastqIO & in) {
+    HashedFastqSet hfqSet;
+    std::vector<std::future<SketchPair>> sketchFutureVec;
+    FastqIO::READ_RESULT res = (in.canRead() ? FastqIO::READ_PASS : FastqIO::READ_EOF);
+    while( res == FastqIO::READ_PASS) {
+        hfqSet.templateVec.emplace_back();
+        ExtendedFastqTemplate_t & fqt = hfqSet.templateVec.back();
+        res = in.next_template(fqt);
+        if(res == FastqIO::READ_PASS) {
+            std::future<SketchPair> future = threadPool_.push(
+                    [&fqt,this](int id) {
+                        SketchPair sp;
+                        return this->GeneratePairedSketch(fqt);
+                    } );
+            sketchFutureVec.push_back(std::move(future));
+        } else if(res != FastqIO::READ_EOF){
+            throw std::invalid_argument("Improperly formatted fastq input");
+        }
+    }
+    size_t fqtIdx = 0;
+    for(auto & future : sketchFutureVec){
+        const SketchPair & sp = future.get();
+        ExtendedFastqTemplate_t & fqt = hfqSet.templateVec[fqtIdx];
+        fqt.meanQual = CalculateMeanQuality(fqt);
+        hfqSet.sketchMap.insert(2*fqtIdx+0,sp.first);
+        hfqSet.sketchMap.insert(2*fqtIdx+1,sp.second);
+    }
+    return hfqSet;
 }
--- src/SketchHashFactory.h
+++ src/SketchHashFactory.h
@@ -1,17 +1,44 @@
 #ifndef SKETCH_HASH_FACTORY_HEADER_GAURD_
 #define SKETCH_HASH_FACTORY_HEADER_GAURD_
 
-#include "Sketcher.h"
-#include "LocalSyncmerMap.h"
-#include "FastqIO.h"
+#include <ctpl_stl.h>
+#include <Sketcher.h>
+#include <LocalSyncmerMap.h>
+#include <FastqIO.h>
+#include <vector>
 
+struct ExtendedFastqTemplate_t : public FastqTemplate_t {
+    double meanQual = 0.0;
+};
 
 struct HashedFastqSet {
+    LocalSyncmerMap sketchMap;
+    std::vector<ExtendedFastqTemplate_t> templateVec;
+};
 
+struct SketchPair {
+    Sketch first;
+    Sketch second;
 };
 
 class SketchHashFactory {
-    SketchHashFactory();
+    public:
+    SketchHashFactory() = delete;
+    SketchHashFactory(  Sketcher && sketcher,size_t nProcThread = 1,
+                        int phredOffset = 33);
+    
+    HashedFastqSet BuildHashedFastqSet(FastqIO & in);
+    protected:
+    //Members
+    Sketcher sketcher_;
+    ctpl::thread_pool threadPool_;
+    const int phredOffset_;
+
+    //Methods
+    HashedFastqSet NoWorkerBuild(FastqIO & in);
+    HashedFastqSet ParallelBuild(FastqIO & in);
+    SketchPair GeneratePairedSketch(const FastqTemplate_t & fqt);
+    double CalculateMeanQuality(const FastqTemplate_t & fqt);
 };
 
 #endif // SKETCH_HASH_FACTORY_HEADER_GAURD_
