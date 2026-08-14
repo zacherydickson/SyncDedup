ccd4e4052c64111cbf9b528d6f169528f86c86af
Zachery Dickson <dicksoz@mcmaster.ca>
2026-08-15 00:39:55 +0300

    Add explicit desctuctors

--- src/FastqTemplateSource.h
+++ src/FastqTemplateSource.h
@@ -19,11 +19,12 @@ struct FastqTemplateSource {
     virtual bool operator()(FastqTemplate_t & fqt) = 0;
     virtual std::vector<FastqTemplate_t> get_block(size_t max_n) = 0;
     virtual bool get_size(size_t & size) const = 0;
+    virtual ~FastqTemplateSource(){}
 };
 
-//TODO: Add Tests
 struct FastqIOAsFQTSource : public FastqTemplateSource {
     FastqIOAsFQTSource(std::shared_ptr<FastqIO> handler);
+    ~FastqIOAsFQTSource() {}
     bool operator()(FastqTemplate_t & fqt) override;
     std::vector<FastqTemplate_t> get_block(size_t max_n) override;
     bool get_size(size_t & size) const override { return false; }
@@ -32,9 +33,9 @@ struct FastqIOAsFQTSource : public FastqTemplateSource {
     FastqIO::READ_RESULT getTemplate(FastqTemplate_t & fqt);
 };
 
-//TODO: Add Tests
 struct VectorAsFQTSource : public FastqTemplateSource {
     VectorAsFQTSource(std::shared_ptr<std::vector<FastqTemplate_t>> vector);
+    ~VectorAsFQTSource() {}
     bool operator()(FastqTemplate_t & fqt) override;
     std::vector<FastqTemplate_t> get_block(size_t max_n) override;
     bool get_size(size_t & size) const override { size = vec->size(); return true; }
