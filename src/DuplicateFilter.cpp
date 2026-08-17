42d006ecf8adf6437f524c0e70f9cdcc410c554d
Zachery Dickson <dicksoz@mcmaster.ca>
2026-08-17 23:32:17 +0300

    Begin implementation: Generating sketch pairs, finding candidates (partial)

--- src/DuplicateFilter.cpp
+++ src/DuplicateFilter.cpp
@@ -1,13 +1,60 @@
 #include <DuplicateFilter.h>
 
-bool DuplicateFilter::operator()(   const ExtendedFastqTemplate_t & fqt,
-                                    const LocalSyncmerMap & lsMap) const
+bool DuplicateFilter::operator()(   const Sketcher & sketcher, 
+                                    size_t fqtIdx,
+                                    const HashedFastqSet & hfqSet) const
 {
-    //TODO: Implement Me
+#ifndef NDEBUG
+    if(fqtIdx > hfqSet.templates.size()) {
+        throw std::logic_error("Attempt to use a Duplicate filter on an unknown template index " + std::to_string(fqtIdx));
+    }
+#endif
+    const FastqTemplate_t & fqt = hfqSet.templates[fqtIdx];
+    bool bPaired = fqt.segVec.size() > 1;
+    SketchPair sp = GeneratePairedSketch(sketcher,fqt);
     //Find candidate matches for the template
-    //Construct synteny blocks from candidates
-    //Filter on excessive indels
-    //Filter on excessive subs
+    HitCandidateMap candMap = FindCandidateHits(fqtIdx,bPaired,sp,hfqSet); 
+    //TODO: Construct synteny blocks from candidates
+    //TODO: Filter on excessive indels
+    //TODO: Filter on excessive subs
     return true;
+}
+
+
+DuplicateFilter::HitCandidateMap DuplicateFilter::FindCandidateHits(
+        size_t fqtIdx, bool bPaired, const SketchPair & sp,
+        const HashedFastqSet & hfqSet )
+{
+    //TODO: Finish Implementation
+    HitCandidateMap candMap;
+    for(uint8_t parity = 0; parity <= (bPaired ? 1 : 0); parity++){
+        const Sketch & sketch = (parity == 0) ? sp.first : sp.second;
+        for(const SketchElement & se : sketch) {
+            std::vector<size_t> locPosVec;
+            for(const LocationElement & le : hfqSet.sketchMap.at(se)) {
+                //Skip hits with different parity
+                if( HashedFastqSet::skparity(le.id) != parity ) { continue; }
+                //Skip the template itself, after noting its location
+                size_t curFqtIdx = HashedFastqSet::sk2fqt(le.id);
+                if( curFqtIdx == fqtIdx ) {
+                    locPosVec.push_back(le.pos);
+                    continue;
+                }
+                std::vector<size_t> & candVec = candMap[curFqtIdx]
+            }
+        }
+    }
+    return candMap;
+}
+
+SketchPair DuplicateFilter::GeneratePairedSketch(   const Sketcher & sketcher, 
+                                                    const FastqTemplate_t & fqt) 
+{
+    SketchPair sp;
+    sp.first = sketcher.generate_sketch(fqt.segVec[0].seq); 
+    if(fqt.segVec.size() > 1){
+        sp.second = sketcher.generate_sketch(fqt.segVec[1].seq); 
+    }
+    return sp;
 }
 
--- src/DuplicateFilter.h
+++ src/DuplicateFilter.h
@@ -1,8 +1,9 @@
 #ifndef DUPLICATE_FILTER_HEADER_GAURD_
 #define DUPLICATE_FILTER_HEADER_GAURD_
 
-#include <SketchHashFactory.h>
 #include <LocalSyncmerMap.h>
+#include <SketchHashFactory.h>
+#include <unordered_map>
 
 class DuplicateFilter {
     public:
@@ -12,11 +13,20 @@ class DuplicateFilter {
         DuplicateFilter(double subRate, double indelRate) 
             : substitutionRate_(subRate), indelRate_(indelRate) {}
 
-        bool operator()(const ExtendedFastqTemplate_t & fqt,
-                        const LocalSyncmerMap & lsMap) const;
+        bool operator()(const Sketcher & sketcher,
+                        size_t fqtIdx,
+                        const HashedFastqSet & hfqSet) const;
     protected:
         const double substitutionRate_ = 0.01;
         const double indelRate_ = 0.0001;
+
+        using HitCandidate = std::pair<std::vector<size_t>,std::vector<size_t>>;
+        using HitCandidateMap = std::unordered_map<size_t,HitCandidate>;
+        static HitCandidateMap FindCandidateHits(
+                    size_t fqtIdx, bool bPaired, const SketchPair & sp,
+                    const HashedFastqSet & hfqSet );
+        static SketchPair GeneratePairedSketch( const Sketcher & sketcher, 
+                                                const FastqTemplate_t & fqt);
 
 };
 
